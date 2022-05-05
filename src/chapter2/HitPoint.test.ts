d3332b20e8b70a8d7cd57452ee11226d7c3001c2
shimopino <shimoke4869@gmail.com>
2022-05-06 00:42:33 +0900

    ファイル名にバージョンが抜けていたので修正

--- src/chapter2/HitPoint.test.ts
+++ src/chapter2/HitPoint.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { HitPointV1 } from './HitPoint';
+import { HitPointV1 } from './HitPointV1';
 
 /**
  * 第2章 データとロジックを近い場所に配置する
@@ -7,15 +7,33 @@ import { HitPointV1 } from './HitPoint';
  * 書籍を上から読むだけだと目的がよくわからないからTDDしづらい...
  */
 describe('HitPoint', () => {
-  it('HPを計算する', () => {
-    const hp = new HitPointV1(100, 50, 20);
+  describe('V1', () => {
+    it('HPを計算する', () => {
+      const hp = new HitPointV1(100, 50, 20);
 
-    hp.recover();
+      hp.recover();
 
-    expect(hp.hitPoint).toBe(100 + 20);
+      expect(hp.hitPoint).toBe(100 + 20);
 
-    hp.damage();
+      hp.damage();
 
-    expect(hp.hitPoint).toBe(100 + 20 - 50);
+      expect(hp.hitPoint).toBe(100 + 20 - 50);
+    });
+
+    it('HPの回復が上限を超える', () => {
+      const hp = new HitPointV1(900, 0, 100);
+
+      hp.recover();
+
+      expect(hp.hitPoint).toBe(999);
+    });
+
+    it('HPの減少が下限を下回る', () => {
+      const hp = new HitPointV1(100, 110, 0);
+
+      hp.damage();
+
+      expect(hp.hitPoint).toBe(0);
+    });
   });
 });
