ebb4f492405fb92bd059583891b5173cd4197dcc
shimopino <shimoke4869@gmail.com>
2022-05-06 02:29:05 +0900

    ダメージ計算の検証を追加

--- src/chapter4/reassign/DamageLogic.test.ts
+++ src/chapter4/reassign/DamageLogic.test.ts
@@ -12,7 +12,17 @@ describe('DamageLogic', () => {
 
       expect(logic).toBeDefined();
     });
+
+    it('ダメージを計算できる', () => {
+      const member = new Member();
+      const enemy = new Enemy();
+      const logic = new DamageLogicV1(member, enemy);
+
+      expect(logic.damage()).toBe(0);
+    });
   });
+
+  describe('V2', () => {
     it.todo('安全な状態で初期化できる');
 
     it.todo('ダメージを計算できる');
