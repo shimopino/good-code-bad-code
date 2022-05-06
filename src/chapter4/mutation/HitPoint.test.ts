b6de62f77522459c7bc1b596923efa94d50bab3c
shimopino <shimoke4869@gmail.com>
2022-05-06 18:52:38 +0900

    満たすべき仕様に関するコメントを追加

--- src/chapter4/mutation/HitPoint.test.ts
+++ src/chapter4/mutation/HitPoint.test.ts
@@ -1,7 +1,20 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { MemberV1 } from './MemberV1';
 
+/**
+ * 満たすべき使用は以下の2つ
+ *
+ * 1. ヒットポイントは0以上
+ * 2. ヒットポイントが0になった場合、死亡状態にする
+ */
 describe('HitPoint', () => {
   describe('V1', () => {
-    it('メンバーの体力が0を下回った場合', () => {});
+    it('メンバーの体力が0を下回った場合', () => {
+      const member = new MemberV1(100);
+
+      member.damage(110);
+
+      expect(member).toEqual(new MemberV1(-10));
+    });
   });
 });
