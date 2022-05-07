92940fd6346a94e19eb156983420587c158297c9
shimopino <shimoke4869@gmail.com>
2022-05-07 15:40:52 +0900

    各else if の中からreturnする

--- src/chapter6/elsereturn/ElseReturn.test.ts
+++ src/chapter6/elsereturn/ElseReturn.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
-import { ElseLogicV1 } from './ElseLogicV1';
 import { Member } from './Member';
+import { ElseLogicV1 } from './ElseLogicV1';
+import { ElseLogicV2 } from './ElseLogicV2';
 
 describe('EarlyReturn', () => {
   describe('V1', () => {
@@ -15,6 +16,22 @@ describe('EarlyReturn', () => {
       expect(dangerHp.getCondition()).toBe('danger');
 
       const deadHp = new ElseLogicV1(new Member(0, 100));
+      expect(deadHp.getCondition()).toBe('dead');
+    });
+  });
+
+  describe('V2', () => {
+    it('ELSE IFの中から return する', () => {
+      const findHp = new ElseLogicV2(new Member(100, 100));
+      expect(findHp.getCondition()).toBe('fine');
+
+      const cautionHp = new ElseLogicV2(new Member(40, 100));
+      expect(cautionHp.getCondition()).toBe('caution');
+
+      const dangerHp = new ElseLogicV2(new Member(20, 100));
+      expect(dangerHp.getCondition()).toBe('danger');
+
+      const deadHp = new ElseLogicV2(new Member(0, 100));
       expect(deadHp.getCondition()).toBe('dead');
     });
   });
