75f3d8b471400239be6947369de7739ab130d692
shimopino <shimoke4869@gmail.com>
2022-05-07 15:10:20 +0900

    第5章で検証コードが間違っていたので修正

--- src/chapter5/methodchain/MethodChain.test.ts
+++ src/chapter5/methodchain/MethodChain.test.ts
@@ -1,12 +1,14 @@
-import { describe } from 'vitest';
+import { describe, it } from 'vitest';
 import { Equipment } from './Equipment';
 import { Equipments } from './Equipments';
 
 describe('MethodChain', () => {
   describe("Tell, Don't Ask", () => {
-    const equipments = new Equipments();
-    const newArmor = new Equipment();
+    it('オブジェクトとして分離して、コミュニケーションを通じて状態変化', () => {
+      const equipments = new Equipments();
+      const newArmor = new Equipment();
 
-    equipments.equipArmor(newArmor);
+      equipments.equipArmor(newArmor);
+    });
   });
 });
