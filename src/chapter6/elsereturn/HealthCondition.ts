3d309ff7e41a30cc79cfc9ac41684a7f13a83387
shimopino <shimoke4869@gmail.com>
2022-05-07 15:50:27 +0900

    定数の型が string になっており文字列リテラルではないので修正

--- src/chapter6/elsereturn/HealthCondition.ts
+++ src/chapter6/elsereturn/HealthCondition.ts
@@ -3,6 +3,6 @@ export const HEALTH_CONDITION = {
   DANGER: 'danger',
   CAUTION: 'caution',
   FINE: 'fine',
-};
+} as const;
 export type HEALTH_CONDITION =
   typeof HEALTH_CONDITION[keyof typeof HEALTH_CONDITION];
